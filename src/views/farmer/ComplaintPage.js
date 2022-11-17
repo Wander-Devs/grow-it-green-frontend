1a4c861503da740ebc400153a5380064e1a92be9
Jayson <jsequino@77global.biz>
2022-11-17 17:09:10 +0800

    admin and farmer fixed initial

---
+++ src/views/farmer/ComplaintPage.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+// react component that copies the given text inside your clipboard
+import { CopyToClipboard } from "react-copy-to-clipboard";
+// reactstrap components
+import { Container, Button, Form, FormGroup, Label, Input } from "reactstrap";
+// core components
+import Header from "components/Headers/Header.js";
+
+const ComplaintPage = () => {
+  const [copiedText, setCopiedText] = useState();
+  return (
+    <>
+      <Header />
+      {/* Page content */}
+      <Container className="mt-4" fluid>
+        {/* Table */}
+        <Form>
+          <FormGroup>
+            COMPLAINT ID
+            <Input
+              id="text"
+              name="tip number"
+              placeholder="complaint id"
+              type="text"
+            />
+          </FormGroup>{" "}
+          <FormGroup>
+            FARMER ID
+            <Input
+              id="text"
+              name="farmer id"
+              placeholder="farmer id"
+              type="text"
+            />
+          </FormGroup>{" "}
+          <FormGroup>
+            <Label for="text">COMPLAINT</Label>
+            <Input id="Text" name="text" type="textarea" />
+          </FormGroup>
+          <Button
+            className="mt-4"
+            color="success"
+            // onClick={() => {
+            //   history.push("/admin/index");
+            // }}
+          >
+            SUBMIT
+          </Button>
+        </Form>
+      </Container>
+    </>
+  );
+};
+
+export default ComplaintPage;
